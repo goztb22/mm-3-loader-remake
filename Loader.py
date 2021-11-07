d6d7bff46331a6087c87cef62bec91f10e7c5637
goztb <80268473+goztb22@users.noreply.github.com>
2021-11-07 17:07:09 +0300

    Add files via upload

---
+++ Loader.py
@@ -0,0 +1,23 @@
+import time
+a = """
+$$$ goztbware $$$
+   Loader v0.1
+  Goztbware 007
+"""
+
+print(a)
+print("| Found process")
+time.sleep(5)
+print("| Hooked up")
+time.sleep(3)
+print("| Downloading goztbware.dll")
+time.sleep(2)
+print("| Downloading goztbware.dll")
+time.sleep(2)
+print("| Downloading goztbware.dll")
+time.sleep(2)
+print("| Injecting goztbware.dll")
+time.sleep(4)
+print("| Injected goztbware!")
+input()
+
